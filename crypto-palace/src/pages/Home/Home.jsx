f1b4af1fbb69549390df6e60471c4c6bb36e056e
Jafar-Rizvi <jafar7rizvi@gmail.com>
2026-01-22 21:55:31 +0530

    Fix typo in welcome message on Home page

--- crypto-palace/src/pages/Home/Home.jsx
+++ crypto-palace/src/pages/Home/Home.jsx
@@ -34,7 +34,7 @@ const Home = () => {
     <div className='home'>
       <div className="hero">
         <h1>Largest <br /> Crypto Mraketplace</h1>
-        <p>Welcome to the worlds largest cryptocurrency marketplace. Sign uo to explore more about cryptos</p>
+        <p>Welcome to the worlds largest cryptocurrency marketplace. Sign up to explore more about cryptos</p>
         <form onSubmit={searchHandler} >
           <input onChange={inputHandler}list='coinlist' value={input} type="text" required placeholder='Search crypto...' />
 
